ea57924ccbf68887b9e2bf93dd90df4cff87172f
Ralph Wendt <ralphmadriaga@gmail.com>
2025-07-24 14:56:06 +0100

    chore: update README

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
 # snatch.nvim
-A sneaky Neovim plugin to snatch file info and stash it in your clipboard like a ninja.
+
+A sneaky Neovim plugin to snatch file info and stash it in your clipboard like a ninja. 🥷
+
+## Features
+
+- **Copy full file path** - Get the absolute path of the current file
+- **Copy filename only** - Get just the filename without the path
+- **Copy relative path** - Get the path relative to your home directory
+- **Smart notifications** - Get feedback when operations succeed or fail
+- **Customizable keymaps** - Configure your own keybindings
+- **User commands** - Use commands like `:SnatchFullPath`, `:SnatchFileName`, and `:SnatchRelativePath`
+
+## Installation
+
+### Using [lazy.nvim](https://github.com/folke/lazy.nvim)
+
+```lua
+{
+  "ralph/snatch.nvim",
+  config = function()
+    require("snatch").setup({
+      keymaps = {
+        full_path = "<leader>cp",     -- Copy full path
+        filename = "<leader>cf",      -- Copy filename
+        relative_path = "<leader>cr"  -- Copy relative path
+      }
+    })
+  end
+}
+```
+```
+
+### Using [packer.nvim](https://github.com/wbthomason/packer.nvim)
+
+```lua
+use {
+  "ralph/snatch.nvim",
+  config = function()
+    require("snatch").setup({
+      keymaps = {
+        full_path = "<leader>cp",
+        filename = "<leader>cf",
+        relative_path = "<leader>cr"
+      }
+    })
+  end
+}
+```
+```
+
+### Using [vim-plug](https://github.com/junegunn/vim-plug)
+
+```vim
+Plug 'ralph/snatch.nvim'
+```
+
+Then add to your init.lua or init.vim:
+```lua
+require("snatch").setup({
+  keymaps = {
+    full_path = "<leader>cp",
+    filename = "<leader>cf",
+    relative_path = "<leader>cr"
+  }
+})
+```
+
+## Usage
+
+### Keymaps (Default)
+
+- `<leader>cp` - Copy the full file path to clipboard
+- `<leader>cf` - Copy just the filename to clipboard
+- `<leader>cr` - Copy the relative file path to clipboard
+
+### Commands
+
+- `:SnatchFullPath` - Copy the full file path to clipboard
+- `:SnatchFileName` - Copy just the filename to clipboard
+- `:SnatchRelativePath` - Copy the relative file path to clipboard
+
+### Examples
+
+If you have a file open at `/home/user/projects/myapp/src/main.lua`:
+
+- Using `<leader>cp` will copy: `/home/user/projects/myapp/src/main.lua`
+- Using `<leader>cf` will copy: `main.lua`
+- Using `<leader>cr` will copy: `~/projects/myapp/src/main.lua`
+
+## Configuration
+
+You can customize the keymaps by passing a config table to the setup function:
+
+```lua
+require("snatch").setup({
+  keymaps = {
+    full_path = "<leader>cp",     -- Default: <leader>cp
+    filename = "<leader>cf",      -- Default: <leader>cf
+    relative_path = "<leader>cr"  -- Default: <leader>cr
+  }
+})
+```
+
+## Requirements
+
+- Neovim 0.8.0 or higher
+- A clipboard provider (like `clipboard=unnamedplus` or `clipboard=unnamed`)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details.
+
+## Contributing
+
+Feel free to open issues or submit pull requests to improve this plugin!
--- lua/snatch/init.lua
+++ lua/snatch/init.lua
@@ -7,8 +7,18 @@ function M.full_path()
 		return
 	end
 
-	vim.fn.setreg("+", path)
-	vim.notify("Full file path copied to clipboard", vim.log.levels.INFO)
+	-- Copy to both system clipboard (+) and default register (") so 'p' works
+	local success = pcall(function()
+		vim.fn.setreg('"', path) -- Also copy to default register for 'p' command
+	end)
+
+	if success then
+		vim.notify("Full file path copied to clipboard: " .. path, vim.log.levels.INFO)
+	else
+		-- Fallback to system clipboard if + register fails
+		vim.fn.setreg('"', path) -- Also copy to default register for 'p' command
+		vim.notify("Full file path copied to system clipboard: " .. path, vim.log.levels.INFO)
+	end
 end
 
 function M.filename()
@@ -18,8 +28,39 @@ function M.filename()
 		return
 	end
 
-	vim.fn.setreg("+", filename)
-	vim.notify("File name copied to clipboard", vim.log.levels.INFO)
+	-- Copy to both system clipboard (+) and default register (") so 'p' works
+	local success = pcall(function()
+		vim.fn.setreg('"', filename) -- Also copy to default register for 'p' command
+	end)
+
+	if success then
+		vim.notify("File name copied to clipboard: " .. filename, vim.log.levels.INFO)
+	else
+		-- Fallback to system clipboard if + register fails
+		vim.fn.setreg('"', filename) -- Also copy to default register for 'p' command
+		vim.notify("File name copied to system clipboard: " .. filename, vim.log.levels.INFO)
+	end
+end
+
+function M.relativeProjectRoot()
+	local relative_path = vim.fn.expand("%:~:.")
+	if relative_path == "" then
+		vim.notify("No file is currently open", vim.log.levels.WARN)
+		return
+	end
+
+	-- Copy to both system clipboard (+) and default register (") so 'p' works
+	local success = pcall(function()
+		vim.fn.setreg('"', relative_path) -- Also copy to default register for 'p' command
+	end)
+
+	if success then
+		vim.notify("Relative file path copied to clipboard: " .. relative_path, vim.log.levels.INFO)
+	else
+		-- Fallback to system clipboard if + register fails
+		vim.fn.setreg('"', relative_path) -- Also copy to default register for 'p' command
+		vim.notify("Relative file path copied to system clipboard: " .. relative_path, vim.log.levels.INFO)
+	end
 end
 
 function M.setup(config)
@@ -27,17 +68,37 @@ function M.setup(config)
 		config = {}
 	end
 
-	vim.keymap.set("n", config.keymaps.full_path, M.full_path, {
+	-- Default keymaps
+	local keymaps = config.keymaps or {
+		full_path = "<leader>cp",
+		filename = "<leader>cf",
+		relative_path = "<leader>cr"
+	}
+
+	-- Set up keymaps
+	vim.keymap.set("n", keymaps.full_path, M.full_path, {
+		silent = true,
+		noremap = true,
 		desc = "Snatch the full file path"
 	})
-	vim.keymap.set("n", "<leader>cf", config.keymaps.filename, {
+	vim.keymap.set("n", keymaps.filename, M.filename, {
+		silent = true,
+		noremap = true,
 		desc = "Snatch the file name"
+	})
+	vim.keymap.set("n", keymaps.relative_path, M.relativeProjectRoot, {
+		silent = true,
+		noremap = true,
+		desc = "Snatch the relative file path from project root"
 	})
 end
 
+-- Create user commands
 vim.api.nvim_create_user_command("SnatchFullPath", M.full_path, { desc = "Snatch the full file path" })
 vim.api.nvim_create_user_command("SnatchFileName", M.filename, { desc = "Snatch the file name" })
+vim.api.nvim_create_user_command("SnatchRelativePath", M.relativeProjectRoot, { desc = "Snatch the relative file path" })
 
+-- Auto-setup with default config
 M.setup()
 
 return M
